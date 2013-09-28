a9b51d6c4480affb15d6aa66e843d21b5a903383
diycomputerscience <admin@diycomputerscience.com>
2013-09-28 16:41:38 +0530

    Fixed code to make it appropriate for the exercise

--- src/com/diycomputerscience/minesweeper/Board.java
+++ src/com/diycomputerscience/minesweeper/Board.java
@@ -8,16 +8,11 @@ public class Board {
 	private Square squares[][];
 	
 	public Board() {
-		this.squares = new Square[MAX_ROWS][MAX_COLS];
-		for(int row=0; row<MAX_ROWS; row++) {
-			for(int col=0; col<MAX_COLS; col++) {
-				squares[row][col] = new Square();
-			}
-		}
+		
 	}
 	
 	public Square[][] getSquares() {
-		return this.squares;		
+			
 	}
 
 }
